69dce8a319906cc66fb3b5dd28b6034c1c38b918
uj151133 <103241727+uj151133@users.noreply.github.com>
2024-01-14 00:50:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import discord
 import os
 from keep_alive import keep_alive
+from google.oauth2.service_account import Credentials
+import gspread
+
+scopes = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
+]
+
+credentials = Credentials.from_service_account_file(
+    "./windy-winter-407404-2140fc1dd60b.json/",
+    scopes=scopes
+)
+
+gc = gspread.authorize(credentials)
 
 client = discord.Client(intents=discord.Intents.default())
 
